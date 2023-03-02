2fce0d64e04e42f7947cf0746d359430f6498852
preeti <preetipatil019@gmail.com>
2023-03-02 14:40:08 -0800

    LinkedIn linked

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -73,7 +73,7 @@ const Navbar = () => {
             <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
                 <ul>
                     <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 ">
-                        <a className="flex justify-between items-center w-full text-gray-300" href="/">Linkedin<FaLinkedin size={30} /></a>
+                        <a className="flex justify-between items-center w-full text-gray-300" href="https://www.linkedin.com/in/preeti-patil-bb433885/">Linkedin<FaLinkedin size={30} /></a>
                     </li>
                     <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333] ">
                         <a className="flex justify-between items-center w-full text-gray-300" href="/">Github<FaGithub size={30} /></a>
