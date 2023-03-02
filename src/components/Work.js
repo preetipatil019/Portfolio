e46615687c5304e72cdf65fec4c2d6dea7bbc53c
preeti <preetipatil019@gmail.com>
2023-03-02 13:45:21 -0800

    PortFolio Website initial commit

---
+++ src/components/Work.js
@@ -0,0 +1,62 @@
+import React from "react";
+import WorkImg from "../assets/Swiggy_img.jpg"
+import YoutubeImg from "../assets/Youtube_img.jpg"
+
+const Work = () => {
+    return (
+    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+        <div className="max-w-[1000px] mx-auto flex flex-col justify-center  w-full h-full">
+            <div className="pb-8">
+                <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600" >Work</p>
+                <p className="py-6">Check out some of my recent work</p>
+            </div>
+            {/* card container*/}
+            <div  className="grid sm:grid-col-2 md:grid-col-3 gap-4">
+                {/* card item -grid item*/}
+                <div style={{backgroundImage:`url(${WorkImg})`}} className="bg-no-repeat shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+                 {/* hover effect */}
+                    <div className="opacity-0 group hover:opacity-100">
+                        <span className="text-4xl font-bold tracking-wider font-sans">
+                            React JS Application
+                        </span>
+                        <div className="pt-8 text-center">
+                           <a href="/">
+                                <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                                    Demo
+                                </button>
+                            </a>
+                            <a href="/">
+                                <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                                    Code
+                                </button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                {/* card2 */}
+                 <div style={{backgroundImage:`url(${YoutubeImg})`}} className="bg-no-repeat shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+                 {/* hover effect */}
+                    <div className="opacity-0 group hover:opacity-100">
+                        <span className="text-4xl font-bold tracking-wider font-sans">
+                            React JS Application
+                        </span>
+                        <div className="pt-8 text-center">
+                           <a href="/">
+                                <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                                    Demo
+                                </button>
+                            </a>
+                            <a href="/">
+                                <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                                    Code
+                                </button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>)
+}
+
+export default Work;
