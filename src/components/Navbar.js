eebbb4687db1db393fa3d6a8ff1694b4c4bb798b
preeti <preetipatil019@gmail.com>
2023-03-05 08:23:16 -0800

    links to resume and email

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -76,13 +76,13 @@ const Navbar = () => {
                         <a className="flex justify-between items-center w-full text-gray-300" href="https://www.linkedin.com/in/preeti-patil-bb433885/">Linkedin<FaLinkedin size={30} /></a>
                     </li>
                     <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333] ">
-                        <a className="flex justify-between items-center w-full text-gray-300" href="/">Github<FaGithub size={30} /></a>
+                        <a className="flex justify-between items-center w-full text-gray-300" href="https://github.com/preetipatil019">Github<FaGithub size={30} /></a>
                     </li>
                      <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0] ">
-                        <a className="flex justify-between items-center w-full text-gray-300" href="/">Email<HiOutlineMail size={30} /></a>
+                        <a className="flex justify-between items-center w-full text-gray-300" href="mailto:preetipatil019@gmail.com">Email<HiOutlineMail size={30} /></a>
                     </li>
                     <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69] ">
-                        <a className="flex justify-between items-center w-full text-gray-300" href="/">Resume<BsFillPersonLinesFill size={30} /></a>
+                        <a className="flex justify-between items-center w-full text-gray-300" href="https://drive.google.com/file/d/1TL_iAS8ZDdfqD4BQVNx8EFJ8SGXOk-RF/view?usp=sharing">Resume<BsFillPersonLinesFill size={30} /></a>
                     </li>
                 </ul>
 
